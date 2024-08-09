611a3471e3b89f7be99f0a9ccc4cc53afc908cfd
kumaryash459 <iaskumaryash@gmail.com>
2024-08-09 19:50:34 +0530

    api is added

--- crypto/script.js
+++ crypto/script.js
@@ -1,4 +1,20 @@
-document.body.style.backgroundImage = "url('./images/background.png')";
-document.body.style.backgroundRepeat = "no-repeat"
-document.body.style.backgroundSize = "cover";
+document.body.style.backgroundColor = '#414141'
+let btc = document.getElementById('Bitcoin');
+let eth = document.getElementById('ethereum');
+let doge = document.getElementById('dogecoin');
+const settings = {
+	async: true,
+	crossDomain: true,
+	url: 'https://coingecko.p.rapidapi.com/exchanges/%7Bid%7D',
+	method: 'GET',
+	headers: {
+		'x-rapidapi-key': 'aefe40ea21mshafa0ee94d7c5d09p176994jsn93d268985b77',
+		'x-rapidapi-host': 'coingecko.p.rapidapi.com'
+	}
+};
 
+$.ajax(settings).done(function (response) {
+	btc.innerHTML = response.bitcoin.inr
+	etc.innerHTML = response.ethereum.inr
+	doge.innerHTML = response.dogecoin.inr
+});
